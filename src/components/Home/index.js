3fb5a79cf265b7cb0dbd3bea9407a56b9d3b6ed3
jwHubor <3287566106@qq.com>
2019-02-11 15:07:08 +0800

    [home样式]

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,8 @@
 import React,{ Component} from 'react';
 import { Input,Carousel } from 'antd';
 import './index.scss';
+
+
 const Search = Input.Search;
 class Home extends Component{
 
@@ -9,24 +11,45 @@ class Home extends Component{
             <div>
                 <div className="top">
                 <span className="top1">
-                    图标
+                    
                 </span>
                 <Search
                     placeholder="活灵魂干红葡萄酒"
                     onSearch={value => console.log(value)}
-                    style={{ width: 260 }}
+                    style={{ width: 230 }}
                     />
-                <span className="top2">图标</span>
+                <span className="top2"></span>
                 </div>
                 <div className="lunbo">
                
                     <Carousel autoplay>
-                        <div><h3>1</h3></div>
+                        <div className="lunbo2"><h3>1</h3></div>
                         <div><h3>2</h3></div>
-                        <div><h3>3</h3></div>
+                        <div className="lunbo2"><h3>3</h3></div>
                         <div><h3>4</h3></div>
                     </Carousel>
                     
+                </div>
+                <div className="circle-columns">
+                    <ul>
+                        <li><a href="/"><div className="tu2019"></div>
+                        <div className="zi2019">
+                            会员商城
+                            </div>
+                            </a></li>
+                    </ul>
+                </div>
+
+                <div className="news">
+
+                </div>
+
+                <div className="kuajing">
+
+                </div>
+
+                <div className="xinpin">
+
                 </div>
             </div>
             
